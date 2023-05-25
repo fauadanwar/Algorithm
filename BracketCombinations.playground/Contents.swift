5898fb952a071b37a8f6db7c70603a3331b85eca
fanwar-onshape <fanwar@ptc.com>
2023-05-25 19:20:37 +0530

    Added function to  check if in a string s containing just the characters '(', ')', '{', '}', '[' and ']'  is valid or not

--- BracketCombinations.playground/Contents.swift
+++ BracketCombinations.playground/Contents.swift
@@ -53,3 +53,30 @@ func SearchingChallenge(_ str: String) -> Int  {
 }
 print(SearchingChallenge("(c(oder)) b(yte)"))
 
+//    Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+//    An input string is valid if:
+//    Open brackets must be closed by the same type of brackets.
+//    Open brackets must be closed in the correct order.
+//    Every close bracket has a corresponding open bracket of the same type.
+    func isValidParentheses(_ s: String) -> Bool {
+        var bracketArray = [Character] ()
+        var bracketDictionary:[Character:Character] = ["}": "{", ")": "(", "]": "["]
+        for char in s
+        {
+            if let bracket = bracketDictionary[char]
+            {
+                if bracketArray.isEmpty == true || bracketArray.removeLast() != bracket
+                {
+                    return false
+                }
+            }
+            else
+            {
+                bracketArray.append(char)
+            }
+        }
+        return bracketArray.count == 0 ? true : false
+    }
+
+    print(isValidParentheses("()[]{}"))
+
