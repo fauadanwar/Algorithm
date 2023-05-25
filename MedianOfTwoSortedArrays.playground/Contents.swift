747d6112870aa8c58ba5a057857a7ef5343086e2
fanwar-onshape <fanwar@ptc.com>
2023-05-25 13:32:15 +0530

    removed comment line

--- MedianOfTwoSortedArrays.playground/Contents.swift
+++ MedianOfTwoSortedArrays.playground/Contents.swift
@@ -3,8 +3,6 @@ Problem: Median of Two Sorted Arrays of diffrent size
 
  Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
 
-The overall run time complexity should be O(log (m+n)).
-
 Example 1:
 
 Input: nums1 = [1,3], nums2 = [2]
