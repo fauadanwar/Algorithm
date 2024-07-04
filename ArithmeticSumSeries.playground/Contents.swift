33bf96d5f794a0c273004d42b5fbda288ef1ee5d
Fouad Anwar <fauad.anwar@gmail.com>
2024-07-04 22:03:45 +0530

    added comment

--- ArithmeticSumSeries.playground/Contents.swift
+++ ArithmeticSumSeries.playground/Contents.swift
@@ -1,3 +1,28 @@
+//Find the sequence sum
+//Problem statement :
+//
+//Rohit loves to play with sequences so he has given you a sequence to solve. To prove to him that you are a good coder, you accept the challenge. Find the sum of the sequence. Given three integers i,j,k find i+(i+1)+(i+2)+(i+3)…j+(j-1)+(j-2)+(j-3)……..k
+//
+//CONSTRAINTS:
+//
+//-108<=i,j,k<=108
+//i,k<=j
+//SAMPLE CASE0 :
+//
+//0 → i=0
+//
+//5 → j=5
+//
+//-1 → k=-1
+//
+//SAMPLE OUTPUT 0:
+//
+//24
+//
+//EXPLANATION 0:
+//
+//0+1+2+3+4+5+4+3+2+1+0-1 =24
+
 import Foundation
 
 func sequenceSum(i: Int, j: Int, k: Int) -> Int {
@@ -24,5 +49,5 @@ print(result)  // Output should be 24
 
 // Sample case
 let sum = sequenceSum(i: 2, j: 9, k: -4)
-print(sum) // Output: 24
+print(sum) // Output: 70
 
