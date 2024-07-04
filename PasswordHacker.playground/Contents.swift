75a7c236a405e7b6650f2d924f79777c0e78414c
Fouad Anwar <fauad.anwar@gmail.com>
2024-07-04 19:28:15 +0530

    Decrypt String from Alphabet to Integer Mapping

---
+++ PasswordHacker.playground/Contents.swift
@@ -0,0 +1,40 @@
+//You are given a string s formed by digits and '#'. We want to map s to English lowercase characters as follows:
+//
+//Characters ('a' to 'i') are represented by ('1' to '9') respectively.
+//Characters ('j' to 'z') are represented by ('10#' to '26#') respectively.
+//Return the string formed after mapping.
+//
+//The test cases are generated so that a unique mapping will always exist.
+//
+// 
+//
+//Example 1:
+//
+//Input: s = "10#11#12"
+//Output: "jkab"
+//Explanation: "j" -> "10#" , "k" -> "11#" , "a" -> "1" , "b" -> "2".
+//Example 2:
+//
+//Input: s = "1326#"
+//Output: "acz"
+
+import Foundation
+func freqAlphabets(_ s: String) -> String {
+    var result = ""
+    var i = 0
+    var stringArray = Array(s).map { String($0) }
+    let dictionary = ["1":"a", "2":"b", "3":"c", "4":"d", "5":"e", "6":"f", "7":"g", "8":"h", "9":"i", "10":"j", "11":"k", "12":"l", "13":"m", "14":"n", "15":"o", "16":"p", "17":"q", "18":"r", "19":"s", "20":"t", "21":"u", "22":"v", "23":"w", "24":"x", "25":"y", "26":"z"]
+    while i < stringArray.count {
+        if i + 2 < stringArray.count && stringArray[i+2] == "#" {
+            result += dictionary[stringArray[i] + stringArray[i+1]]!
+            i += 3
+        } else {
+            result += dictionary[stringArray[i]]!
+            i += 1
+        }
+    }
+    return result
+}
+
+
+print(freqAlphabets("10#11#12"))
